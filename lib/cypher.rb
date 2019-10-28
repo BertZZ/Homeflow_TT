ed437a155c813234a60dc05133ce4dddc2269005
BertZZ <bertie.wooles@gmail.com>
2019-10-28 22:17:11 +0000

    Passes first 2 tests

--- lib/cypher.rb
+++ lib/cypher.rb
@@ -1,6 +1,10 @@
 class CaesarCypher
 
   def self.encrypt(string,key)
-    'b'
+    letters = ('a'..'z')
+    encrypted_string = ''
+
+    string.each_char {|char| encrypted_string << char.ord+key}
+    encrypted_string
   end
 end
--- spec/caesar_cypher_spec.rb
+++ spec/caesar_cypher_spec.rb
@@ -7,5 +7,9 @@ describe CaesarCypher do
     it 'returns the next letter in the alphabet when given a key of 1' do
       expect(CaesarCypher.encrypt('a',1)).to eq('b')
     end
+
+    it 'returns a letter 2 away when given a key of 2' do
+      expect(CaesarCypher.encrypt('a',2)).to eq('c')
+    end
   end
 end
